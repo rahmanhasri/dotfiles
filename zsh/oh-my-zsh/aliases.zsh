948f0e0c432b13fefc2643c9536b8eb4fb7ab633
rahmanhasri <rahman.hasri@gmail.com>
2025-12-26 18:30:29 +0800

    edit zsh folder

---
+++ zsh/oh-my-zsh/aliases.zsh
@@ -0,0 +1,14 @@
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias zshsource="source ~/.zshrc"
+alias tmuxconfig="nvim ~/.tmux.conf"
+alias ghosconfig="nvim ~/Library/Application\ Support/com.mitchellh.ghostty/config"
+
+alias lifepal="cd ~/Documents/projects/lifepal"
+
+
+alias gpoh="git push origin HEAD"
+alias ws="/usr/local/bin/windsurf"
+alias n="nvim"
+alias markdown="nvim /Users/mamanmac/Dropbox/markdown"
+
