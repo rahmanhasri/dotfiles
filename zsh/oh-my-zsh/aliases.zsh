6d763d90d38699276b79ade4783e6c9baccf81e4
Rahman Hasri <44260183+rahmanhasri@users.noreply.github.com>
2026-08-12 00:05:43 +0800

    Update aliases.zsh

--- zsh/oh-my-zsh/aliases.zsh
+++ zsh/oh-my-zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias zshconfig="vim ~/.zshrc"
-alias ohmyzsh="vim ~/.oh-my-zsh"
 alias zshsource="source ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
 alias tmuxconfig="nvim ~/.tmux.conf"
 alias ghosconfig="nvim ~/Library/Application\ Support/com.mitchellh.ghostty/config"
 
@@ -8,7 +8,17 @@ alias lifepal="cd ~/Documents/projects/lifepal"
 
 
 alias gpoh="git push origin HEAD"
+alias find=gfind
+
 alias ws="/usr/local/bin/windsurf"
+
+
 alias n="nvim"
 alias markdown="nvim /Users/mamanmac/Dropbox/markdown"
-
+alias gwt='git worktree'
+alias gwta='git worktree add'
+alias gwtl='git worktree list'
+alias gwtr='git worktree remove'
+alias gwtp='git worktree prune'
+alias code='npx cmd'
+alias omni='fnm use v26 && omniroute'
